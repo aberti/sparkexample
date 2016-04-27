cbb41ec6ca0d7c776c5883aedb1250fb7ec328b4
aberti <aber@91.pl>
2016-04-27 13:17:41 +0200

    synch

--- src/main/java/sparkexample/Hello.java
+++ src/main/java/sparkexample/Hello.java
@@ -1,12 +1,25 @@
 package sparkexample;
 
+import java.util.Map;
+
 import static spark.Spark.get;
 
 public class Hello {
 
     public static void main(String[] args) {
+
         get("/", (req, res) -> {
             return "hello from sparkjava.com";
         });
+
+        get("/envs", (req, res) -> {
+            StringBuilder buf = new StringBuilder();
+            Map<String, String> env = System.getenv();
+            for (String envName : env.keySet()) {
+                buf.append(envName + " : " + env.get(envName) + "<br/>");
+            }
+            return buf.toString();
+        });
     }
+
 }
