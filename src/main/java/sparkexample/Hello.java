0bb2046c81ffe6fcb9f2432b5bb8e29f79db7484
aberti <aber@91.pl>
2016-04-27 21:14:09 +0200

    redis

--- src/main/java/sparkexample/Hello.java
+++ src/main/java/sparkexample/Hello.java
@@ -1,15 +1,29 @@
 package sparkexample;
 
+import redis.clients.jedis.Jedis;
+
+import java.io.IOException;
+import java.net.Socket;
+import java.net.UnknownHostException;
 import java.util.Map;
 
 import static spark.Spark.get;
 
 public class Hello {
 
+
+    static final String REDIS_ADDR = "redis";
+    static final int REDIS_PORT = 6379;
+
+    private static boolean connected = false;
+    static Socket s;
+
+    private static Jedis jedis = null;
+
     public static void main(String[] args) {
 
         get("/", (req, res) -> {
-            return "hello world - 4";
+            return "hello world (called: " + getCounter() + " times )";
         });
 
         get("/envs", (req, res) -> {
@@ -20,6 +34,53 @@ public class Hello {
                 return "brak env HOSTNAME";
         });
 
+    }
+
+    static void conectToRedis() {
+        if (hostAvailabilityCheck()) {
+            jedis = new Jedis(REDIS_ADDR, REDIS_PORT);
+            jedis.connect();
+        }
+    }
+
+    static String getCounter() {
+
+        if (jedis == null) {
+            conectToRedis();
+        }
+
+        if (jedis != null) {
+            String counter = jedis.get("counter");
+            if (counter == null) {
+                counter = "1";
+            }
+            return jedis.incr("counter").toString();
+        } else {
+            return "---NO REDIS---";
+        }
+    }
+
+
+    static boolean hostAvailabilityCheck() {
+
+        boolean available = true;
+        try {
+            if (connected == false) {
+                (s = new Socket(REDIS_ADDR, REDIS_PORT)).close();
+            }
+        } catch (UnknownHostException e) { // unknown host
+            available = false;
+            s = null;
+        } catch (IOException e) { // io exception, service probably not running
+            available = false;
+            s = null;
+        } catch (NullPointerException e) {
+            available = false;
+            s = null;
+        }
+
+
+        return available;
     }
 
 }
